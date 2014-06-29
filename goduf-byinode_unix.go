ac5fcecf8c0d54555d8663e0598e422a36f92015
Mikael Berthe <mikael@lilotux.net>
2014-06-29 17:52:53 +0200

    Change comments so that build constraints still work

--- goduf-byinode_unix.go
+++ goduf-byinode_unix.go
@@ -1,11 +1,10 @@
-/*
- * Copyright (C) 2014 Mikael Berthe <mikael@lilotux.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- */
+//
+// Copyright (C) 2014 Mikael Berthe <mikael@lilotux.net>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or (at
+// your option) any later version.
 
 // +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris
 
